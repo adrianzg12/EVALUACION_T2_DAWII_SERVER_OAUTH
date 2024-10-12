62ee3bec20b4131c6857dccbd7264cb8a076d7d5
AdrianLlontop <rizzaga1234@gmail.com>
2024-10-11 22:52:30 -0500

    server terminado

---
+++ src/main/java/pe/edu/cibertec/DAWIISpringSecurityServer/DawiiSpringSecurityServerApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWIISpringSecurityServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiSpringSecurityServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiSpringSecurityServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DAWIISpringSecurityServer/DawiiSpringSecurityServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWIISpringSecurityServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiiSpringSecurityServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
